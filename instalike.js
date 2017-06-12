f1cd01468304911326f6cb23af6a11167c63b79b
Andrés Maldonado <andresio_mo@hotmail.com>
2017-06-12 05:30:46 +0200

    Create instalike.js

--- instalike.js
+++ instalike.js
@@ -11,8 +11,30 @@ var simulateClick = function (elem) {
 var nextFrame = document.querySelector('a._de018.coreSpriteRightPaginationArrow');
 var likeInstagram = document.querySelector('span._soakw.coreSpriteLikeHeartOpen');
 
-setInterval(function(){
-	simulateClick(nextFrame);
-	simulateClick(likeInstagram);
-	
-}, 13000);
+function goLike() {
+	var yesOrNot = (Math.floor(Math.random() * 2));
+	switch(yesOrNot){
+		case 0:
+			break;
+       	 	case 1:
+			simulateClick(likeInstagram);
+			simulateClick(nextFrame);		
+			break;
+        }	
+}
+
+(function loop() {
+    var rand = Math.round(Math.random() * (3000)) + 1000;
+    setTimeout(function() {
+            goLike();
+            loop();  
+    }, rand);
+}());
+
+//var nextFrame = document.querySelector('a._de018.coreSpriteRightPaginationArrow');
+//var likeInstagram = document.querySelector('span._soakw.coreSpriteLikeHeartOpen');
+
+//setInterval(function(){
+//	simulateClick(nextFrame);
+//	simulateClick(likeInstagram);
+//}, 13000);
