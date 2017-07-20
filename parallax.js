9adbdec479020a29cfeef664632a63a06dd1e831
Andrés Maldonado <andresio_mo@hotmail.com>
2017-07-20 03:17:04 +0200

    Create parallax.js

---
+++ parallax.js
@@ -0,0 +1,11 @@
+	(function(){
+		var parallax = document.querySelectorAll(".parallax"),
+		speed = 0.5;
+		window.onscroll = function(){
+			[].slice.call(parallax).forEach(function(el,i){
+				var windowYOffset = window.pageYOffset,
+				BackgrounPos = "50% " + (windowYOffset * speed) + "px";
+				el.style.backgroundPosition = BackgrounPos;
+			});
+		};
+	})();
