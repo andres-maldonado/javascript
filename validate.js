b51ef6ec706395f731b68988f23ae77d60010598
Andrés Maldonado <andresio_mo@hotmail.com>
2017-07-23 17:21:19 +0200

    Create validate.js

---
+++ validate.js
@@ -0,0 +1,7 @@
+**VALIDATE NUMBER :**
+
+!/^([0-9])*$/.test(numero)
+
+! = permite establecer un negativo, condicion que no se cumple.
+/^([0-9])*$/ = acepta cualquier elemento entre 0 y 9.
+.test(numero) = valida la sintaxis.
