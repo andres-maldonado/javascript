ff432cab83afcbfe394b018605283a8587727bbd
Andrés Maldonado <andresio_mo@hotmail.com>
2017-10-17 13:47:59 -0300

    Update instalike.js

--- instalike.js
+++ instalike.js
@@ -1,4 +1,4 @@
-var rightFrame = document.querySelector('a._de018.coreSpriteRightPaginationArrow');
+var rightFrame = document.querySelector('a._3a693.coreSpriteRightPaginationArrow');
 var likeInstagram = document.querySelector('span._soakw.coreSpriteHeartOpen');
 var leftFrame = document.querySelector('a._qdy3e.coreSpriteLeftPaginationArrow');
 var max_number = Math.round(Math.random()*(20)+30);
